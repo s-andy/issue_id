8c508d7e445c5f691756b8629019e396433832ce
Andriy Lesyuk <s-andy@andriylesyuk.com>
2013-11-02 23:13:04 +0000

    Added Bulgarian translation, thanks user#88 (#2232)
    git-svn-id: http://svn.s-andy.com/issue-id@15 8a93503b-87fb-43fc-9c53-525b9925230c

--- lib/issue_id_hook.rb
+++ lib/issue_id_hook.rb
@@ -4,5 +4,4 @@ class IssueIdHook  < Redmine::Hook::ViewListener
     render_on :view_issues_sidebar_issues_bottom,   :partial => 'issues/title'
     render_on :view_issues_show_description_bottom, :partial => 'issues/id'
 
-    # TODO view_issues_bulk_edit_details_bottom (replace ul with issue IDs)
 end
